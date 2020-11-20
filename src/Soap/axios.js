015f66aebba55aec4c841096442be1c8348ae124
mafecarbo <30605015+mafecarbo@users.noreply.github.com>
2020-11-20 12:51:14 -0500

    Soap Preparacion

--- src/Soap/axios.js
+++ src/Soap/axios.js
@@ -2,16 +2,28 @@ const axios_ = require('axios');
 const apiUrl = require('./apiGraph').apiUrl;
 const queries = require('./apiGraph').queries;
 
+
+
 process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0';
 
-axios_.post(apiUrl, {
+async function getUserByEmail(email){
+  axios_.post(apiUrl, {
     query: queries.getUserByEmail
-})
-.then((res) => {
+  })
+  .then((res) => {
     console.log("Entre")
-  console.log(res.data)
-})
-.catch((error) => {
+    console.log(res.data.data.getUserByEmail)
+    return res.data.data.getUserByEmail
+  })
+  .catch((error) => {
     console.log(" NO Entre")
-  console.error(error)
-});
+    console.error(error)
+    return "No se encontro usuario"
+  });
+}
+
+module.exports = {
+  getUserByEmail
+}
+
+
