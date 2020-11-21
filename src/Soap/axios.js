c7d2243bee87476387342624d9e2219428f8b4a0
RENE <alrramirezso@unal.edu.co>
2020-11-20 22:49:10 -0500

    funciona completamente el soap probado

--- src/Soap/axios.js
+++ src/Soap/axios.js
@@ -3,9 +3,9 @@ const apiUrl = 'https://3.90.88.218:5000/graphql';
 process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0';
 
 async function getUserByEmail(email){
-  axios_.post(apiUrl, {
+  return await axios_.post(apiUrl, {
     query: ` query{
-      getUserByEmail(email: "${email}"){
+      getUserByEmail(email: ${email}){
         id
         email
         name
@@ -18,7 +18,14 @@ async function getUserByEmail(email){
     if(res.data.data.getUserByEmail != null && res.status == 200){
       console.log("axios")
       console.log(res.data.data.getUserByEmail)
-      return res.data.data.getUserByEmail;
+      let users = {
+        id: res.data.data.getUserByEmail.id,
+        email: res.data.data.getUserByEmail.email,
+        name: res.data.data.getUserByEmail.name,
+        nickname: res.data.data.getUserByEmail.nickname,
+        image: res.data.data.getUserByEmail.image
+      }
+      return {users:users};
   }
   else{
     console.log("Usuario no encontrado");
@@ -32,6 +39,8 @@ async function getUserByEmail(email){
   });
 }
 
+/*console.log("el retorno");
+console.log(getUserByEmail("elusuariodeprueba2@unal.edu.co"));*/
 module.exports = {
   getUserByEmail
 }
