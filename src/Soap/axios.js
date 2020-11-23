c3e4da44a6669ad715e5e19c4ae2703918186c76
RENE <alrramirezso@unal.edu.co>
2020-11-22 21:34:11 -0500

    commit before pull funcional

--- src/Soap/axios.js
+++ src/Soap/axios.js
@@ -1,5 +1,5 @@
 const axios= require('axios');
-const apiUrl = 'https://3.90.88.218:5000/graphql';
+const apiUrl = 'https://3.90.88.218/graphql';
 process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0';
 async function getUserByEmail(email){
   return await axios.post(apiUrl, {
@@ -43,5 +43,3 @@ console.log(getUserByEmail("elusuariodeprueba2@unal.edu.co"));*/
 module.exports = {
   getUserByEmail
 }
-
-
